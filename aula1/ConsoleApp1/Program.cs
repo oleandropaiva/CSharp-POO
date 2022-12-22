a947885f9a090662af897bfb5eade8d66164891a
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-21 21:18:27 -0300

    exemplo sbyte, byte, long e int

--- aula1/ConsoleApp1/Program.cs
+++ aula1/ConsoleApp1/Program.cs
@@ -7,7 +7,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            
+
             Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
         }
     }
 }
