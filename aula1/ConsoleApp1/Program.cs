88c28ec72557f222c4526b9993c74db40e94ac90
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-21 21:38:40 -0300

    char, bool

--- aula1/ConsoleApp1/Program.cs
+++ aula1/ConsoleApp1/Program.cs
@@ -6,12 +6,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            bool completo = false;
+            char genero = 'F';
             byte n1 = 126;
             int n2 = 1000;
             int n3 = 2147483647;
             long n4 = 2147483648L;
-            
 
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
