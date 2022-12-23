b641374c9166b85fefa170ffae41d26e5b3e5c1a
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-22 23:09:26 -0300

    concatenação

--- aula2/aula2/Program.cs
+++ aula2/aula2/Program.cs
@@ -16,6 +16,8 @@ namespace Aula2
             Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo );
 
             Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
     
         }
     }
