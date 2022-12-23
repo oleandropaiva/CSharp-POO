452a565d97c6a5b2f6bf6c2e55379ec9609645ba
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-22 22:40:55 -0300

    usando placeholder

--- aula2/aula2/Program.cs
+++ aula2/aula2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+            
+
+            Console.Write("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo );
+    
+        }
+    }
+}
