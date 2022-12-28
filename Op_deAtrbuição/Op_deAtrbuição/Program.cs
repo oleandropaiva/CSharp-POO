4d4c1dd2168c8ef5a17dccbc2080d7a3896700cb
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-28 18:21:02 -0300

    imprme na tela e CultureInfo.InvariantCulture

--- Op_deAtrbuição/Op_deAtrbuição/Program.cs
+++ Op_deAtrbuição/Op_deAtrbuição/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace opAtribuição
 {
@@ -6,9 +7,23 @@ namespace opAtribuição
     {
         static void Main(string[] args)
         {
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
             Console.Write("Bom dia!");
             Console.WriteLine("Boa tarde!");
             Console.WriteLine("Boa noite");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F3"));
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
         }
     }
 }
