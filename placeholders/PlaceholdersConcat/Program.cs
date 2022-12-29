fb6761a38b35efe3494abb1c812a65071a20026e
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-29 09:44:47 -0300

    tec da concatenação

--- placeholders/PlaceholdersConcat/Program.cs
+++ placeholders/PlaceholdersConcat/Program.cs
@@ -15,7 +15,9 @@ namespace opAtribuição
             Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
 
             Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
-            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais");
+
 
         }
     }
