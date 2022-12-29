1ca56582f48339a5dd640060263373122e873a5d
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-29 13:18:53 -0300

    exemplo de atribuição de int a double usando o CASTING

---
+++ ConversaoAndCasting/ConversaoAndCasting/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConversaoAndCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            int b;
+
+            a = 5.1;
+            b = (int)a; // isso é o casting
+
+            Console.WriteLine(b);
+        }
+    }
+}
