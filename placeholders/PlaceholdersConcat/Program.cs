2cdffac619542afb0b819322a57c7f1e6def5f53
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-29 09:13:01 -0300

    tecnica d placeholder

--- placeholders/PlaceholdersConcat/Program.cs
+++ placeholders/PlaceholdersConcat/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace opAtribuição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais", nome, idade, saldo);
+           
+        }
+    }
+}
