1506b541b5292697a90fd5b48f17507455a65351
Leandro Paiva <leandroapaiva@yahoo.com>
2022-12-30 21:28:11 -0300

    implementando Split

--- EntradaDeDados/Program.cs
+++ EntradaDeDados/Program.cs
@@ -12,13 +12,21 @@ namespace EntradaDeDados
             string y = Console.ReadLine();
             string z = Console.ReadLine();
 
+            string s = Console.ReadLine();
+
+            string[] v = s.Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
             Console.WriteLine("Você digitou: ");
             Console.WriteLine(frase);
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(z);
-            Console.WriteLine(Console.ReadLine());
-
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
         }
     }
 }
