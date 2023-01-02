f459285fc30d69eb64f174811cdf16a27fe18ace
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-02 15:31:53 -0300

    imprime numero usando CultureInfo.InvariantCulture

--- EntradaDeDados/Program.cs
+++ EntradaDeDados/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace EntradaDeDados
 {
@@ -8,12 +9,12 @@ namespace EntradaDeDados
         {
             int n1 = int.Parse(Console.ReadLine());
             char ch = char.Parse(Console.ReadLine());
-            double n2 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Você digitou");
+            Console.WriteLine("Você digitou: ");
             Console.WriteLine(n1);
             Console.WriteLine(ch);
-            Console.WriteLine(n2);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
