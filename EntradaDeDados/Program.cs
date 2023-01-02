5f9684f90c25585503a4b75da429362f6f4656b0
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-02 14:55:38 -0300

    conversão de tipos int.parse

--- EntradaDeDados/Program.cs
+++ EntradaDeDados/Program.cs
@@ -1,30 +1,15 @@
 ﻿using System;
 
-
 namespace EntradaDeDados
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
+            int n1 = int.Parse(Console.ReadLine());
 
-            string[] v = Console.ReadLine().Split(' ');
-            string a = v[0];
-            string b = v[1];
-            string c = v[2];
-
-            Console.WriteLine("Você digitou: ");
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            Console.WriteLine("Você digitou");
+            Console.WriteLine(n1);
         }
     }
 }
