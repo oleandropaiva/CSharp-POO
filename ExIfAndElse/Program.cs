2641d7b7c96666759895881a8edfae7bf2c9caea
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-04 23:31:39 -0300

    determina qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem

--- ExIfAndElse/Program.cs
+++ ExIfAndElse/Program.cs
@@ -7,32 +7,45 @@ namespace ExIfAndElse
     {
         static void Main(string[] args)
         {
-            double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            string[] valores = Console.ReadLine().Split(' ');
+            double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
 
-            if (numero < 0.0 || numero > 100.0)
+            if (x == 0.0 && y == 0.0)
             {
-                Console.WriteLine("Fora de intervalo");
+                Console.WriteLine("Origem");
             }
-            else if (numero <= 25.0)
+            else if (x == 0.0)
             {
-                Console.WriteLine("Intervalo [0,25]");
+                Console.WriteLine("Eixo Y");
             }
-            else if (numero <= 50.0)
+            else if (y == 0.0)
             {
-                Console.WriteLine("Intervalo (25,50]");
+                Console.WriteLine("Eixo X");
             }
-            else if (numero <= 75.0)
+            else if (x > 0.0 && y > 0.0)
             {
-                Console.WriteLine("Intervalo (50,75]");
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q3");
             }
             else
             {
-                Console.WriteLine("Intervalo (75,100]");
+                Console.WriteLine("Q4");
             }
         }
     }
 }
 
-// Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
-// seguintes intervalos([0,25], (25,50], (50, 75], (75, 100]) este valor se encontra.
-// Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+/* Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+Se o ponto estiver na origem, escreva a mensagem “Origem”.
+Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
+situação. */
