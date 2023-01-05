cde8943138c628ee348a91ab51a204251c0fa75f
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-04 22:13:28 -0300

    encadeamento if and else

--- IfAndElse/Program.cs
+++ IfAndElse/Program.cs
@@ -5,16 +5,20 @@
         static void Main(string[] args)
                         
         {
-            Console.WriteLine("Entre com um número inteiro: ");
-            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a hora atual? ");
+            int hora = int.Parse(Console.ReadLine());
 
-            if (x % 2 == 0)
+            if (hora < 12)
             {
-                Console.WriteLine("Par!");
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
             }
             else
             {
-                Console.WriteLine("Impar!");
+                Console.WriteLine("Boa noite!");
             }
         }
     }
