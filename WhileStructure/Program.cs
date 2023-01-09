c1762c6972f3c8a8ae29a1dad56e6ed4b9ea6d7b
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-09 16:20:01 -0300

    WhileStructure

---
+++ WhileStructure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WhileStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
