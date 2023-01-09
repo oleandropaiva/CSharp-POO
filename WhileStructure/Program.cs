fef7037ce0d0b95495bd2ed594745909c81b4417
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-09 16:28:30 -0300

    estrutura de raiz de um número

--- WhileStructure/Program.cs
+++ WhileStructure/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace WhileStructure
+﻿using System;
+using System.Globalization;
+
+namespace WhileStructure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Digite um número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+            
         }
     }
 }
