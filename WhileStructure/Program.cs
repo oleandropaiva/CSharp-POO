4c0a616edbf9923dcc741562c1947793d64f4501
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-09 16:37:55 -0300

    estrutura de raiz de um número com while

--- WhileStructure/Program.cs
+++ WhileStructure/Program.cs
@@ -9,9 +9,15 @@ namespace WhileStructure
         {
             Console.Write("Digite um número: ");
             double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
 
-            double raiz = Math.Sqrt(x);
-            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
             
         }
     }
