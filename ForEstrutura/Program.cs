dceb31348d31b5bb750398a29ba3c85037582e95
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-16 20:33:54 -0300

    executando cod

--- ForEstrutura/Program.cs
+++ ForEstrutura/Program.cs
@@ -7,10 +7,15 @@
             Console.Write("Quantos números inteiros você vai digitar? ");
             int N = int.Parse(Console.ReadLine());
 
+            int soma = 0;
             for (int i = 1; i <= N; i++)
             {
-                Console.WriteLine("oi");
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma = soma + valor;
             }
+
+            Console.WriteLine("Soma = " + soma);
         }
     }
 }
