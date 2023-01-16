8069bd060c83ef94fadf45ae8671d020ffbc27da
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-16 20:46:32 -0300

    exercicio for

--- exercicioFor/Program.cs
+++ exercicioFor/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
