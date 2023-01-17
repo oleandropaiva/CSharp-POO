d5e699f07ea007a4f13061fc08efff1e4825d160
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-16 22:07:32 -0300

    resolvendo problema sem POO

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace POO
+﻿using System;
+using System.Globalization;
+
+namespace POO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InstalledUICulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InstalledUICulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior Área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
         }
     }
 }
