e07dbba6fc2b097b0e53764a3f7c358ea95baec2
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-18 21:52:59 -0300

    cria classe Triangulo

--- POO/Triangulo.cs
+++ POO/Triangulo.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POO
+﻿namespace POO
 {
     internal class Triangulo
     {
+        public double A;
+        public double B;
+        public double C;
     }
 }
